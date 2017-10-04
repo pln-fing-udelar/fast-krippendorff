4acd786afd2a50fca6b139dae7ab3eab20290eeb
Santiago Castro <santi.1410@hotmail.com>
2017-10-03 23:18:23 -0300

    Fix ratio measure when comparing two values equal to zero

--- krippendorff/krippendorff.py
+++ krippendorff/krippendorff.py
@@ -27,7 +27,7 @@ def _interval_metric(v1, v2, **_kwargs):
 
 def _ratio_metric(v1, v2, **_kwargs):
     """Metric for ratio data."""
-    return ((v1 - v2) / (v1 + v2)) ** 2
+    return (((v1 - v2) / (v1 + v2)) ** 2) if v1 + v2 != 0 else 0
 
 
 def _coincidences(value_counts, value_domain, dtype=np.float64):
