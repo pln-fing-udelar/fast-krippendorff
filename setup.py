ef4556dc63f73b9edd3084935f12a5fff5d4271b
Santiago Castro <santi.1410@hotmail.com>
2017-10-08 19:03:40 -0300

    Bump version 0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import pypandoc
 
 setup(
     name="krippendorff",
-    version="0.2.0",
+    version="0.2.1",
     description="Fast computation of the Krippendorff's alpha measure.",
     long_description=pypandoc.convert('README.md', 'rst'),
     url="https://github.com/pln-fing-udelar/fast-krippendorff",
