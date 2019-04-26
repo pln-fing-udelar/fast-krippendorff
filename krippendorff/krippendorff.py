8e85b25c0b42daf16cdf955fb2a3d0ea715d386f
Santiago Castro <sacastro@umich.edu>
2019-04-25 20:41:23 -0400

    Fix ordinal metric depends on the order of the inputs

--- krippendorff/krippendorff.py
+++ krippendorff/krippendorff.py
@@ -17,6 +17,8 @@ def _nominal_metric(v1, v2, **_kwargs):
 
 def _ordinal_metric(_v1, _v2, i1, i2, n_v):
     """Metric for ordinal data."""
+    if i1 > i2:
+        i1, i2 = i2, i1
     return (np.sum(n_v[i1:(i2 + 1)]) - (n_v[i1] + n_v[i2]) / 2) ** 2
 
 
