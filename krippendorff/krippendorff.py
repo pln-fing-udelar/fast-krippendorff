c974cad4dad4ff4ad0195f9fb5bfc7b9cb865a3c
Santiago Castro <sacastro@umich.edu>
2019-04-25 20:48:26 -0400

    Add a test for the ratio measure

--- krippendorff/krippendorff.py
+++ krippendorff/krippendorff.py
@@ -222,7 +222,7 @@ def alpha(reliability_data=None, value_counts=None, value_domain=None, level_of_
     ...                          [0, 0, 1, 1]])
     >>> print(round(alpha(value_counts=value_counts, level_of_measurement='nominal'), 6))
     0.691358
-    >>> # The following example comes from
+    >>> # The following examples were extracted from
     >>> # https://www.statisticshowto.datasciencecentral.com/wp-content/uploads/2016/07/fulltext.pdf, page 8.
     >>> reliability_data = [[1, 2, 3, 3, 2, 1, 4, 1, 2, np.nan, np.nan, np.nan],
     ...                     [1, 2, 3, 3, 2, 2, 4, 1, 2, 5, np.nan, 3.],
@@ -230,6 +230,8 @@ def alpha(reliability_data=None, value_counts=None, value_domain=None, level_of_
     ...                     [1, 2, 3, 3, 2, 4, 4, 1, 2, 5, 1, np.nan]]
     >>> print(round(alpha(reliability_data, level_of_measurement='ordinal'), 3))
     0.815
+    >>> print(round(alpha(reliability_data, level_of_measurement='ratio'), 3))
+    0.797
     """
     if (reliability_data is None) == (value_counts is None):
         raise ValueError("Either reliability_data or value_counts must be provided, but not both.")
