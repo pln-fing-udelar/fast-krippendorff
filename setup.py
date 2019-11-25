f9c41117e69004ed0307ea93932d71f8ab8fc22d
Santiago Castro <sacastro@umich.edu>
2019-11-24 20:40:11 -0500

    Remove pypandoc and use setup.cfg for the package information (#8)

--- setup.py
+++ setup.py
@@ -1,34 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from setuptools import find_packages, setup
+from setuptools import setup
 
-import pypandoc
-
-setup(
-    name="krippendorff",
-    version="0.3.2-unreleased",
-    description="Fast computation of the Krippendorff's alpha measure.",
-    long_description=pypandoc.convert_file('README.md', 'rst'),
-    url="https://github.com/pln-fing-udelar/fast-krippendorff",
-    keywords=["Krippendorff", "alpha", "agreement", "reliability", "coding", "coders", "units", "values"],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    ],
-    packages=find_packages(),
-    author="Santiago Castro",
-    author_email="sacastro@fing.edu.uy",
-    maintainer="Santiago Castro",
-    maintainer_email="sacastro@fing.edu.uy",
-    license="GPL 3.0",
-)
+setup()
